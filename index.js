f56f42e6d8543b2be2c2387b70a08602fcc728ce
Patrick McClernan <patmccler@gmail.com>
2017-10-25 22:44:07 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,9 @@
+
+function theBeatlesPlay(musicians, instruments) {
+  var sentences
+
+  for( let i = 0; i < musicians.length; i++){
+    sentences.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
+  return sentences
+}
